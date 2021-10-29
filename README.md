607225ef0e86dd7149f17426c5b74c9d5330d4c9
Akkie100 <stephan.akkerman@gmail.com>
2021-10-29 21:13:53 +0200

    File structure changes

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# Setup
-Follow the link in binance_keys.py to set up your personal Telegram bot, and get your id token so you can send Telegram messages via your bot to yourself.
+# New Binance Listings
+As the name suggests this script sends you an automated message if Binance has just listed a new coin.
 
-# How it works
+## Setup
+Follow the link in src/binance_keys.py to set up your personal Telegram bot, and get your id token so you can send Telegram messages via your bot to yourself.
+
+## How it works
 Every minute (could be faster if you would like) the Binance exchange is checked, if the total amount of symbols listed on their exchange has changed compared to last time a message is send. This message contains the new symbols that got listed on the spot exchange.
+
+## Dependencies
+The required packages to run this code can be found in the `requirements.txt` file. To run this file, execute the following code block:
+```
+$ pip install -r requirements.txt 
+```
+Alternatively, you can install the required packages manually like this:
+```
+$ pip install <package>
+```
+
+## How to run
+- Clone the repository
+- Run `$ python src/main.py`
+- See result
 
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from binance import Client
-import datetime     # Used for converting time to string
-import requests     # Used for telegram messages
-import threading    # Used for scheduling tasks not handled by the websocket
-import math         # Used for calculating the precision for making orders
-import time         # Used for time.sleep
-import traceback    # Used for debugging
+import datetime  # Used for converting time to string
+import requests  # Used for telegram messages
+import threading  # Used for scheduling tasks not handled by the websocket
+import math  # Used for calculating the precision for making orders
+import time  # Used for time.sleep
+import traceback  # Used for debugging
 
-import binance_keys # Save API & Telegram keys here
+import binance_keys  # Save API & Telegram keys here
 
 bot_token = binance_keys.bot_token
 send_to = binance_keys.send_to
@@ -16,9 +16,10 @@ send_to = binance_keys.send_to
 # Binance API keys not necessary
 client = Client()
 
-def sendBuyAlert(bot_message : str):
-    """ 
-    Sends message via Telegram, using tokens specified in binance_keys.py 
+
+def sendBuyAlert(bot_message: str):
+    """
+    Sends message via Telegram, using tokens specified in binance_keys.py
     @param bot_messsage : string with the text message needed to be send
     """
 
@@ -26,32 +27,41 @@ def sendBuyAlert(bot_message : str):
     print(" ".join([bot_message, "at", datetime.datetime.now().strftime("%H:%M:%S")]))
 
     # Send message over Telegram
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + send_to + '&parse_mode=HTML&text=' + bot_message
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id="
+        + send_to
+        + "&parse_mode=HTML&text="
+        + bot_message
+    )
     response = requests.get(send_text)
 
-#This is necessary for newListings
+
+# This is necessary for newListings
 oldSymbols = client.get_exchange_info().get("symbols")
-    
-def newListings():  
+
+
+def newListings():
     """
     Checks if there a new listings and sends a message if there are
     """
 
     try:
-        #Use the global oldSymbols
+        # Use the global oldSymbols
         global oldSymbols
 
         currentSymbols = client.get_exchange_info().get("symbols")
-    
+
         # If symbols get removed, refresh oldSymbols
         if len(currentSymbols) < len(oldSymbols):
-        
-            #Refresh if symbols get deleted
+
+            # Refresh if symbols get deleted
             oldSymbols = client.get_exchange_info().get("symbols")
 
         # If there are new listings, currentLen will be larger
         # The new listings will always be at the end of the list of dicts
-        if len(currentSymbols) > len(oldSymbols):     
+        if len(currentSymbols) > len(oldSymbols):
             diff = len(currentSymbols) - len(oldSymbols)
 
             # Get the last new listings
@@ -62,26 +72,27 @@ def newListings():
 
             # For all new symbols
             for syms in newSymbols:
-                sym = (syms.get("symbol"))
+                sym = syms.get("symbol")
 
-                if "SPOT" in syms.get("permissions"): 
-                    #Always send a message in case of a spot listing
+                if "SPOT" in syms.get("permissions"):
+                    # Always send a message in case of a spot listing
                     sendBuyAlert("New SPOT Listing: " + sym)
 
-        #Check every minute for changes
+        # Check every minute for changes
         threading.Timer(60, newListings).start()
 
-    except Exception as e: 
-        #Print out all the error information
+    except Exception as e:
+        # Print out all the error information
         print(e)
         print(traceback.format_exc())
 
-        #Wait 1 min before retrying
+        # Wait 1 min before retrying
         print("retrying in 60 sec")
-        time.sleep(60) 
+        time.sleep(60)
 
         print("retrying...")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Starting NewListings.py")
     newListings()
